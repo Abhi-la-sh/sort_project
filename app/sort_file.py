2d22d5ff3143e41706a00d7738f08aa551f24820
Abhi-la-sh <abhibrigade@gmail.com>
2023-07-20 13:09:52 +0000

    modification to check number of argumenets

--- app/sort_file.py
+++ app/sort_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import pandas as pd
 import sys
-
+if len(sys.argv)<4:
+    print("usage: sort_file.py inputfname. outputfname sortby")
+    sys.exit()
 infname=sys.argv[1]
 outfname=sys.argv[2]
 sortby=sys.argv[3]
